191077a8940ceec11d2cf6f38ce3c9cd0e7ee8fd
alfian742 <alhidayat742@gmail.com>
2025-04-21 13:52:51 +0800

    Update time zone

--- resources/views/components/schedule-list.blade.php
+++ resources/views/components/schedule-list.blade.php
@@ -1,4 +1,14 @@
 @if ($schedules->count())
+    @php
+        $offset = \Carbon\Carbon::now()->getOffset() / 3600;
+        $zone = match ($offset) {
+            7 => 'WIB',
+            8 => 'WITA',
+            9 => 'WIT',
+            default => 'N/A',
+        };
+    @endphp
+
     <ul class="list-unstyled list-unstyled-border">
         @foreach ($schedules as $schedule)
             <li class="media">
@@ -10,7 +20,7 @@
                         |
                         {{ \Carbon\Carbon::parse($schedule->start_time)->format('H:i') }}
                         -
-                        {{ \Carbon\Carbon::parse($schedule->end_time)->format('H:i') }}
+                        {{ \Carbon\Carbon::parse($schedule->end_time)->format('H:i') }} {{ $zone }}
                     </div>
                 </div>
             </li>
